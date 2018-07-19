b201eceadeeb6427e72b895ee7122bf37e9f88ab
megorov <megorov@central.uh.edu>
2018-07-19 17:34:45 -0500

    graphs wierd output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,30 +13,11 @@ with open('ionosphere.data', 'r') as file:
         y_data.append(line.split(',')[34].strip())
         x_data.append([float(x) for x in line.split(',')[:34]])
 n_splits = [2, 4, 6, 8, 10]
-error_rate = []
 for n in n_splits:
-    kf = KFold(n_splits=n)
-    for train, test in kf.split(x_data):
-        x_train = np.array(x_data)[train]
-        y_train = np.array(y_data)[train]
-        x_test = np.array(x_data)[test]
-        y_test = np.array(y_data)[test]
-        # Fit  model
-        model_1 = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
-                                 algorithm="SAMME",
-                                 n_estimators=200)
-        model_1.fit(x_train, y_train)
+    # Fit  model
+    clf1 = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=1))
+    clf2 = AdaBoostClassifier(base_estimator=KNeighborsClassifier(5))
+    scores = cross_val_score(clf1, x_data, y_data, cv=n)
+    scores2 = cross_val_score(clf2, x_data, y_data, cv=n)
+    print('clf1 scores = ', scores.mean(), ' clf2 scores = ', scores2.mean())
 
-
-        # Predict
-        y_1 = model_1.predict(x_test)
-
-        correct = 0
-        wrong = 0
-        for i,j in zip(y_1, y_test):
-            if i == j:
-                correct += 1
-            else:
-                wrong += 1
-        error_rate.append(wrong /(wrong + correct))
-    print(np.mean(error_rate))
